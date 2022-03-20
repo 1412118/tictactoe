fd752e2b26d108090285e2c67248711b71e7ff14
1412118 <dtdatap@gmail.com>
2022-03-20 17:02:33 +0700

    change state to a object and calculate winner

--- src/Board.js
+++ src/Board.js
@@ -3,39 +3,67 @@ import Square from './Square';
 import {useState} from 'react';
 
 function Board(){
-    let [status, setStatus] = useState('X');
-    //let status = 'X';
-
-    let [states, setStates] = useState(Array(9));
-
-   //let states = Array(9);
+    //let [status, setStatus] = useState('X');
+    let [state, setState] = useState({
+        squares: Array(9).fill(''),
+        xIsNext: true
+    });
 
     function handleOnClick(i){
-        if(status === 'X'){
-            states[i] = 'X';
-            setStatus('O');
+        if(calculateWinner(state.squares)){
+            return;
         }
-        else{
-            states[i] = 'O';
-            setStatus('X'); 
-        }
-        console.log(states);
+        const squares = state.squares.slice();
+        squares[i] = state.xIsNext ? 'X' : 'O';
+        setState({
+            squares,
+            xIsNext: !state.xIsNext
+        })
     }
 
     function renderSquare(i){
         return(
             <Square 
-                value={states[i]}
+                value={state.squares[i]}
                 onClick={() => handleOnClick(i)}
             />
         )
     }
 
+    function calculateWinner(squares){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for(var i = 0; i < lines.length; i++){
+            const [a, b, c] = lines[i];
+            if(squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a]
+            }
+        }
+        return null;
+    }
+
+    function changeStatus(){
+        const winner = calculateWinner(state.squares);
+        if(winner){
+            return 'Winner: ' + winner;
+        }
+        else{
+            return 'Next Player: ' + (state.xIsNext ? 'X' : 'O');
+        }
+    }
+
     return(
         <Fragment>
-            {console.log(1)}
         <div className="board">
-            <h3 className="status">Next Player: {status}</h3>
+            <h3 className="status">{changeStatus()}</h3>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {useState} from 'react';
-import Board from './Board';
 
 function Square(props){
-    console.log(props)
     return(
         <button className="square" onClick={props.onClick}>{props.value}</button>
     )
