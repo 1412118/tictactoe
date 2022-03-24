c4bab6c2a79f5eef644fae8f8a2d58f4ad2cfad2
1412118 <dtdatap@gmail.com>
2022-03-25 01:16:54 +0700

    showing past moves

--- src/Board.js
+++ src/Board.js
@@ -2,68 +2,21 @@ import { Fragment } from 'react';
 import Square from './Square';
 import {useState} from 'react';
 
-function Board(){
+function Board({squares, onClick}){
     //let [status, setStatus] = useState('X');
-    let [state, setState] = useState({
-        squares: Array(9).fill(''),
-        xIsNext: true
-    });
-
-    function handleOnClick(i){
-        if(calculateWinner(state.squares)){
-            return;
-        }
-        const squares = state.squares.slice();
-        squares[i] = state.xIsNext ? 'X' : 'O';
-        setState({
-            squares,
-            xIsNext: !state.xIsNext
-        })
-    }
-
+    
     function renderSquare(i){
         return(
             <Square 
-                value={state.squares[i]}
-                onClick={() => handleOnClick(i)}
+                value={squares[i]}
+                onClick={() => onClick(i)}
             />
         )
-    }
-
-    function calculateWinner(squares){
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for(var i = 0; i < lines.length; i++){
-            const [a, b, c] = lines[i];
-            if(squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a]
-            }
-        }
-        return null;
-    }
-
-    function changeStatus(){
-        const winner = calculateWinner(state.squares);
-        if(winner){
-            return 'Winner: ' + winner;
-        }
-        else{
-            return 'Next Player: ' + (state.xIsNext ? 'X' : 'O');
-        }
     }
 
     return(
         <Fragment>
         <div className="board">
-            <h3 className="status">{changeStatus()}</h3>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,81 @@
 import Board from "./Board";
+import React, {Fragment, useState} from 'react';
 
 function Game(){
-    
+    let [state, setState] = useState({
+        history: [{
+            squares: Array(9).fill('')
+        }],
+        //squares: Array(9).fill(''),
+        xIsNext: true
+    });
+
+    function handleOnClick(i){
+        const stateHistory = state.history;
+        if(calculateWinner(stateHistory[stateHistory.length - 1].squares)){
+            return;
+        }
+        const squares = stateHistory[stateHistory.length - 1].squares.slice();
+        squares[i] = state.xIsNext ? 'X' : 'O';
+        
+        state.history.push({squares});
+        const history = state.history;
+        setState({
+            history,
+            //squares,
+            xIsNext: !state.xIsNext
+        })
+    }
+
+    function calculateWinner(squares){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for(let i = 0; i < lines.length; i++){
+            const [a, b, c] = lines[i];
+            if(squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a]
+            }
+        }
+        return null;
+    }
+
+    function changeStatus(){
+        const history = state.history[state.history.length - 1];
+        const winner = calculateWinner(history.squares);
+        if(winner){
+            return 'Winner: ' + winner;
+        }
+        else{
+            return 'Next Player: ' + (state.xIsNext ? 'X' : 'O');
+        }
+    }
+
     return (
-        <Board />
+        <Fragment>
+            <div>
+                <div className="status">{changeStatus()}</div>
+                <Board 
+                    squares={state.history[state.history.length - 1].squares}
+                    onClick={(i) => handleOnClick(i)}
+                />
+            </div>
+            <ul>
+                {
+                    state.history.map((e, index) => {
+                        const desc = index ? "Go to step " + index : "Go to start game"
+                        return <li>{desc}</li>
+                    })
+                }
+            </ul>
+        </Fragment>
     )
 }
 
